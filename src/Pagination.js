967aac6ce261e66ef951599957754921d137e962
RevanthGovindan <revanthji14@gmail.com>
2018-07-30 18:29:14 +0530

    page

--- src/Pagination.js
+++ src/Pagination.js
@@ -1,17 +1,43 @@
-import React,{Component} from 'react';
-class Pagination extends Component{
-    constructor(props){
+import React, { Component } from 'react';
+class Pagination extends Component {
+    constructor(props) {
         super(props);
-        this.state={data:this.props.data}
-        this.update=this.update.bind(this)
+        this.state = { pageArr: [], currentpage: 0 };
+        this.update = this.update.bind(this);
     }
-    update(){
+    update() {
         this.props.action(5);
     }
-    render(){
-        return(
+    range() {
+        var i;
+        var start = this.state.currentpage;
+        for (i = start; i < start + 10; i++) {
+            this.state.pageArr.push(i);
+        }
+    }
+    prevPage() {
+
+    }
+    nextPage() {
+
+    }
+    render() {
+        return (
             <div>
-                <button onClick={this.update}>click</button>
+                {/* <button onClick={this.update}>click</button> */}
+                <ul>
+                    <li onClick="prevPage()">
+                        <a href="">Previous</a>
+                    </li>
+                    {
+                        this.state.pageArr.map((pages,ind)=><li onClick={this.update(pages)} key={ind}>
+                            {pages}
+                        </li>)
+                    }
+                    <li onClick="nextPage()">
+                        <a href="">Next</a>
+                    </li>
+                </ul>
             </div>
         );
     }
--- src/UsersList.js
+++ src/UsersList.js
@@ -1,9 +1,6 @@
 import React, { Component } from 'react';
 import './UsersList.css';
 class UsersList extends Component {
-    constructor(props) {
-        super(props);
-    }
     render() {
         return (
             <div>
